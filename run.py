c9121b5793de0078b2768c828e669a1e8c630b28
Dom-888 <spinelladomenico88@gmail.com>
2020-04-02 18:06:20 +0000

    Messages are now saved in a list of dictionaries

--- run.py
+++ run.py
@@ -9,11 +9,9 @@ messages = [] # Tutti i messaggi vengono salvati qui da add_message e poi organi
 # Chiamata da send_message
 def add_message(username, message):
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    message_dict = {"timestamp": now, "from": username, "message": message}
 
-# Chiamata da user
-def get_all_messages():
-    return "<br>".join(messages)
+    messages.append(message_dict)
 
 # Appare atterrando sul sito
 @app.route('/', methods = ["GET","POST"]) # Autorizza i metodi get e post in questa pagina
@@ -28,7 +26,7 @@ def index():
 # La funzione finale che appare sullo schermo
 @app.route('/<username>') 
 def user(username):
-    return "<h1>Hi {0}</h1>{1}".format(username, get_all_messages())
+    return "<h1>Hi {0}</h1>{1}".format(username, messages)
 
 # Prende l'input dell'utente e lo inserisce nella lista tramite add_message, poi rinfresca la pagina
 @app.route('/<username>/<message>') # Il testo all'interno delle <> viene passato come argomento nella funzione decorata
